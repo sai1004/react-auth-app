89f8eb319454a8387f0f1272d6cae7b84a02ccb0
sai1004 <saikiran.bavandla@gmail.com>
2020-11-10 22:52:45 +0530

    commit

--- src/components/loginCard.js
+++ src/components/loginCard.js
@@ -1,7 +1,31 @@
 const loginCard = () => {
   return (
     <>
-      <h4> Hello Login Card !!</h4>
+      <div className="logo__section">
+        <h1> Logo </h1>
+      </div>
+      <div className="box">
+        <div>
+          <h4> Login To Your Account </h4>
+        </div>
+        <form>
+          <div className="form__Inputs">
+            <div>
+              <input type="email" placeholder="Enter Your Email" required />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Enter Your password"
+                required
+              />
+            </div>
+          </div>
+          <div className="submit">
+            <button className="btn__submit"> Submit </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -1,10 +1,18 @@
 import "./loginPage.css";
-import loginCard from "../../components/loginCard";
+import LoginCard from "../../components/loginCard";
 
 const loginPage = () => {
   return (
     <div className="bg__img">
-      <h1> Hello to Login Page !!</h1>
+      <div className="login__card">
+        <div className="card__body">
+          <LoginCard />
+          <div className="below__text">
+            <p className="text_one"> Don't have a account? </p>
+            <p className="text_two"> Forgot Password? </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
